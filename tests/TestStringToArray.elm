ae8a033dffc27657fbbbc3741c15fb78bd0109e2
h64m1 <h64ms1@gmail.com>
2020-05-04 16:37:02 +0900

    [add] add elm-test source #10
    - add test scripts in package.json to run elm-test

---
+++ tests/TestStringToArray.elm
@@ -0,0 +1,10 @@
+module TestStringToArray exposing (..)
+
+import Expect exposing (Expectation)
+import Fuzz exposing (Fuzzer, int, list, string)
+import Test exposing (..)
+
+
+suite : Test
+suite =
+    describe ""
